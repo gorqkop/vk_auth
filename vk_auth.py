fe0c8eab643054f49823332c583ee26d34df4a1b
gorqkop <gor.qkop@gmail.com>
2014-04-19 15:46:23 +0300

    Update vk_auth.py

--- vk_auth.py
+++ vk_auth.py
@@ -1,13 +1,13 @@
 __author__ = 'Gor'
-'''For Python 3.x'''
 import urllib.request as ur
 import urllib.parse as up
 import http.cookiejar as hc
 import lxml.html, re
 import requests as r
-#Inspired by http://habrahabr.ru/post/143972/   and    https://github.com/dzhioev/vk_api_auth/blob/master/vk_auth.py
+#http://habrahabr.ru/post/143972/   and    https://github.com/dzhioev/vk_api_auth/blob/master/vk_auth.py
 
 class parser:
+    """Парсим формы в поисках нужных полей для подтверждения вопросов"""
     def __init__(self, response):
         html = lxml.html.fromstring(response)
         self.params = {}
@@ -21,10 +21,14 @@ class parser:
 
 def auth(email, password, client_id, scope):
     opener = ur.build_opener(ur.HTTPCookieProcessor(hc.CookieJar()), ur.HTTPRedirectHandler())
-    response = r.get("http://oauth.vk.com/oauth/authorize?redirect_uri=http://oauth.vk.com/blank.html&response_type=token&client_id=%s&scope=%s&display=wap"%(client_id, scope)).content
+    response = r.get("http://oauth.vk.com/oauth/authorize?redirect_uri=http://oauth.vk.com/blank.html&response_type=token&client_id=%s&scope=%s&display=wap"%(str(client_id), str(scope))).content
     pars = parser(response)
     pars.params["email"] = email
     pars.params["pass"] = password
     res = opener.open(pars.url, up.urlencode(pars.params).encode('cp1251'))
-    key = re.findall('access_token=[a-zA-Z0-9]+', res.geturl())[0][13:]
+    try: key = re.findall('access_token=[a-zA-Z0-9]+', res.geturl())[0][13:]
+    except:
+        pars = parser(res.read())
+        res = opener.open(pars.url, up.urlencode(pars.params).encode('cp1251'))
+        key = re.findall('access_token=[a-zA-Z0-9]+', res.geturl())[0][13:]
     return key
